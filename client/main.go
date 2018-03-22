dac2c876ae26410accf57495590152784c72b104
Jordan Anderson <me@jba.io>
2018-03-22 16:40:31 -0500

    Using an interface for individual checks. checkData is returned by all. Add context to a few log points.

--- client/main.go
+++ client/main.go
@@ -16,20 +16,39 @@ import (
 )
 
 type config struct {
-	loadavg float64
+	maxLoad float64
+}
+
+type checkData struct {
+	issue *gorram.Issue
+	ok    bool
 }
 
 type check interface {
-	doCheck(config) *gorram.Issue
+	doCheck() *checkData
 }
 
-func reportIssues(cfg *config) []*gorram.Issue {
-	var issues []*gorram.Issue
-	issue := loadavg{}.doCheck(cfg)
-	if issue != nil {
-		issues = append(issues, issue)
+// This is where all the actual checks are done, and an array of "issues" are made
+func doChecks(cfg *config) []*gorram.Issue {
+	var checks []*gorram.Issue
+	loadCheck := getCheck(loadavg{maxLoad: cfg.maxLoad})
+	if loadCheck != nil {
+		checks = append(checks, loadCheck)
 	}
-	return issues
+	return checks
+}
+
+// getCheck() is a function which all Checks should run through
+// It should only be called above by doCheck(), which then checks if the issue is nil
+func getCheck(c check) *gorram.Issue {
+	//log.Println("Check:", c)
+	theCheck := c.doCheck()
+	if !theCheck.ok {
+		log.Println("Check is not OK:", theCheck)
+		return theCheck.issue
+	}
+
+	return nil
 }
 
 func main() {
@@ -39,13 +58,9 @@ func main() {
 	interval := flag.Duration("interval", 60*time.Second, "Number of seconds to check for issues on.")
 
 	// These flags are issue-specific
-	loadAvg := flag.Float64("load-avg", 8, "The load average above which to alert on.")
+	maxload := flag.Float64("load-avg", 8, "The load average above which to alert on.")
 
 	flag.Parse()
-
-	cfg := &config{
-		loadavg: *loadAvg,
-	}
 
 	// Catch Ctrl+C, sigint
 	sigs := make(chan os.Signal, 1)
@@ -71,6 +86,10 @@ func main() {
 	// Add secret key metadata
 	ctx = metadata.AppendToOutgoingContext(ctx, "secret", *secretKey)
 
+	cfg := &config{
+		maxLoad: *maxload,
+	}
+
 	// Ping and collect issues every X seconds
 	ticker := time.NewTicker(*interval)
 	quit := make(chan struct{})
@@ -82,9 +101,9 @@ func main() {
 				if err != nil {
 					log.Fatalln(err)
 				}
-				log.Println(ping.GetIsAlive())
+				log.Println("ping is", ping.GetIsAlive())
 
-				i := reportIssues(cfg)
+				i := doChecks(cfg)
 				for _, issue := range i {
 					submitted, err := c.RecordIssue(ctx, issue)
 					if err != nil && err != io.EOF {
