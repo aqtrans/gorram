a747d0678c4da721dfa7a734c5133b7b8c03d4e6
Jordan Anderson <me@jba.io>
2018-03-22 11:51:30 -0500

    Add flags to allow configuring. Add secret to the metadata sent, to allow proper authentication. Add a config struct passed along to hold issue settings.

--- client/main.go
+++ client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"io"
 	"log"
 	"os"
@@ -14,30 +15,41 @@ import (
 	"jba.io/go/gorram/proto"
 )
 
-func reportIssues() []*gorram.Issue {
-	issue1 := &gorram.Issue{
-		Message:       "woooo",
-		TimeSubmitted: time.Now().Unix(),
+type config struct {
+	loadavg float64
+}
+
+func reportIssues(cfg *config) []*gorram.Issue {
+	var issues []*gorram.Issue
+	issue := getLoadAvg(cfg)
+	if issue != nil {
+		issues = append(issues, issue)
 	}
-	issue2 := &gorram.Issue{
-		Message:       "woooo1",
-		TimeSubmitted: time.Now().Unix(),
-	}
-	issue3 := &gorram.Issue{
-		Message:       "woooo2",
-		TimeSubmitted: time.Now().Unix(),
-	}
-	return []*gorram.Issue{issue1, issue2, issue3}
+	return issues
 }
 
 func main() {
+	// Set config via flags
+	serverAddress := flag.String("server-address", "127.0.0.1:50000", "Address and port of the server.")
+	secretKey := flag.String("server-secret", "omg12345", "Secret key of the server.")
+	interval := flag.Duration("interval", 60*time.Second, "Number of seconds to check for issues on.")
+
+	// These flags are issue-specific
+	loadAvg := flag.Float64("load-avg", 8, "The load average above which to alert on.")
+
+	flag.Parse()
+
+	cfg := &config{
+		loadavg: *loadAvg,
+	}
+
 	// Catch Ctrl+C, sigint
 	sigs := make(chan os.Signal, 1)
 	done := make(chan bool, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
 	// Set up a connection to the server.
-	conn, err := grpc.Dial("127.0.0.1:50000", grpc.WithInsecure())
+	conn, err := grpc.Dial(*serverAddress, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
@@ -52,8 +64,11 @@ func main() {
 	// Add client name metadata
 	ctx = metadata.AppendToOutgoingContext(ctx, "client", "client1")
 
-	// Ping and collect issues every 5 seconds
-	ticker := time.NewTicker(5 * time.Second)
+	// Add secret key metadata
+	ctx = metadata.AppendToOutgoingContext(ctx, "secret", *secretKey)
+
+	// Ping and collect issues every X seconds
+	ticker := time.NewTicker(*interval)
 	quit := make(chan struct{})
 	go func() {
 		for {
@@ -65,7 +80,7 @@ func main() {
 				}
 				log.Println(ping.GetIsAlive())
 
-				i := reportIssues()
+				i := reportIssues(cfg)
 				for _, issue := range i {
 					submitted, err := c.RecordIssue(ctx, issue)
 					if err != nil && err != io.EOF {
