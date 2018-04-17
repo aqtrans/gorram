ff1decbc36c3541991bb4379cf56639c35672e60
Jordan Anderson <me@jba.io>
2018-04-16 20:20:48 -0500

    Now properly checking against ExpectedBody if specified.

--- checks/geturl.go
+++ checks/geturl.go
@@ -1,10 +1,14 @@
 package checks
 
 import (
+	"bytes"
 	"fmt"
-	pb "jba.io/go/gorram/proto"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"time"
+
+	pb "jba.io/go/gorram/proto"
 )
 
 type GetURL struct {
@@ -24,6 +28,24 @@ func (g GetURL) doCheck() *checkData {
 			ok: false,
 		}
 	}
+
+	if resp.StatusCode == 200 && g.Cfg.ExpectedBody != "" {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln("error reading body:", err)
+		}
+		if !bytes.Equal(body, []byte(g.Cfg.ExpectedBody)) {
+			return &checkData{
+				issue: &pb.Issue{
+					Title:         "Get URL",
+					Message:       fmt.Sprintf("%v body content is unexpected.", g.Cfg.Url),
+					TimeSubmitted: time.Now().Unix(),
+				},
+				ok: false,
+			}
+		}
+	}
+
 	if resp.StatusCode != 200 {
 		return &checkData{
 			issue: &pb.Issue{
