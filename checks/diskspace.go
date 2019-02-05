8dd0a0948f715d9ee2ca4f0d17ddc2d377b7f109
Jordan Anderson <me@jba.io>
2019-02-05 12:25:49 -0600

    Extend disk space check to allow checking for minimum GB free, using MinFreeGb option

--- checks/diskspace.go
+++ checks/diskspace.go
@@ -23,12 +23,29 @@ func (p DiskSpace) doCheck() string {
 		log.Println("Error getting disk usage for "+p.Cfg.Partition+":", err)
 		return ""
 	}
-	log.Println("DEBUG DiskSpace: ", usage.UsedPercent, usage.String(), usage.Free)
-	if usage.UsedPercent > p.Cfg.MaxUsage {
-		return fmt.Sprintf("Disk usage of %s is greater than %f, %f", p.Cfg.Partition, p.Cfg.MaxUsage, usage.UsedPercent)
+
+	//log.Println("DEBUG DiskSpace: ", usage.UsedPercent, usage.String(), usage.Free)
+
+	if p.Cfg.GetMaxUsage() != 0 {
+		if usage.UsedPercent > p.Cfg.MaxUsage {
+			return fmt.Sprintf("Disk usage of %s is greater than %f, %f", p.Cfg.Partition, p.Cfg.MaxUsage, usage.UsedPercent)
+		}
+		if usage.UsedPercent == p.Cfg.MaxUsage {
+			return fmt.Sprintf("Disk usage of %s is at %f, %f", p.Cfg.Partition, p.Cfg.MaxUsage, usage.UsedPercent)
+		}
 	}
-	if usage.UsedPercent == p.Cfg.MaxUsage {
-		return fmt.Sprintf("Disk usage of %s is at %f, %f", p.Cfg.Partition, p.Cfg.MaxUsage, usage.UsedPercent)
+
+	if p.Cfg.GetMinFreeGb() != 0 {
+		// Calculate the given GB to bytes:
+		minBytes := p.Cfg.MinFreeGb * 1000000000
+		if usage.Free < minBytes {
+			freeGB := usage.Free / 1000000000
+			return fmt.Sprintf("Free space of %s is less than %v, %v", p.Cfg.Partition, p.Cfg.MinFreeGb, freeGB)
+		}
+		if usage.Free == minBytes {
+			freeGB := usage.Free / 1000000000
+			return fmt.Sprintf("Disk usage of %s is at %v, %v", p.Cfg.Partition, p.Cfg.MinFreeGb, freeGB)
+		}
 	}
 
 	return ""
