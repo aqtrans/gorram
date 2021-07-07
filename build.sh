c3d9181a8b6551e32e764e9fdba021e5d4d5670c
Jordan Anderson <me@jba.io>
2021-07-06 23:51:00 -0500

    build_proto() bash function installs necessary protoc compilers.

--- build.sh
+++ build.sh
@@ -26,6 +26,8 @@ function build_package() {
 
 function build_proto()
 {
+    go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.26
+    go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1
     protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative proto/gorram.proto
 }
 
